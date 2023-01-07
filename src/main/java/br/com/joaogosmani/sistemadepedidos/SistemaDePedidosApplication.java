46e52d877cf817cc5b1d63252393991f8aef2ed1
Joao Gosmani <joaogosmani@gmail.com>
2023-01-07 18:34:07 -0300

    Projeto criado

---
+++ src/main/java/br/com/joaogosmani/sistemadepedidos/SistemaDePedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.sistemadepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joaogosmani/sistemadepedidos/SistemaDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.sistemadepedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
