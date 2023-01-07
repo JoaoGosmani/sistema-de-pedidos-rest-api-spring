0140378a729a3ddf1221ae5b9de30b02f96f7077
Joao Gosmani <joaogosmani@gmail.com>
2023-01-07 19:08:53 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/joaogosmani/sistemadepedidos/resources/CategoriaResource.java
+++ src/main/java/br/com/joaogosmani/sistemadepedidos/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.joaogosmani.sistemadepedidos.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.joaogosmani.sistemadepedidos.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 	
 }
